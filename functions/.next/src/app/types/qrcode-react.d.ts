79951969fa18a57bf5fe3ca71aac712773000b8b
David <davidleonar@gmail.com>
2025-11-29 13:59:18 -0500

    DB rules

--- functions/.next/src/app/types/qrcode-react.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'qrcode.react' {
-    export const QRCodeCanvas: React.FC<{ value: string; size?: number; className?: string }>;
-  }
--- functions/.next/src/app/types/window.d.ts
+++
@@ -1,8 +0,0 @@
-// src/types/window.d.ts
-import { RecaptchaVerifier } from 'firebase/auth';
-
-declare global {
-  interface Window {
-    recaptchaVerifier?: RecaptchaVerifier;
-  }
-}
