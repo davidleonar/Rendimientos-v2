c6eff1a3f76ec1c163814a82cc84f0d468ea442a
David <davidleonar@gmail.com>
2025-12-01 19:33:56 -0500

    USDT TRON

--- functions/.next/types/validator.ts
+++ functions/.next/types/validator.ts
@@ -65,6 +65,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../src/app/api/recordUsdtTronDeposit/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/recordUsdtTronDeposit">> = Specific
+  const handler = {} as typeof import("../../src/app/api/recordUsdtTronDeposit/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 
 
 
