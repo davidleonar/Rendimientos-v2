524ad91b30c6ba06ee1d8b5b62c06745cddfa3dd
David <davidleonar@gmail.com>
2026-01-24 22:01:22 -0500

    Polygon Done

--- functions/.next/types/validator.ts
+++ functions/.next/types/validator.ts
@@ -65,15 +65,6 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
-// Validate ../../src/app/api/recordUsdtTronDeposit/route.ts
-{
-  type __IsExpected<Specific extends RouteHandlerConfig<"/api/recordUsdtTronDeposit">> = Specific
-  const handler = {} as typeof import("../../src/app/api/recordUsdtTronDeposit/route.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
 // Validate ../../src/app/api/tapdProxy/[...path]/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/tapdProxy/[...path]">> = Specific
